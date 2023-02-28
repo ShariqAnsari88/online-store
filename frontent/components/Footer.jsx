481d778a996bc994164c2521b978afdb17d0ea85
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-02-28 23:03:32 +0530

    footer responsive

--- frontent/components/Footer.jsx
+++ frontent/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
         <footer className="bg-black text-white pt-14 pb-3">
             <Wrapper className="flex justify-between flex-col md:flex-row gap-[50px] md:gap-0">
                 {/* LEFT START */}
-                <div className="flex gap-[50px] md:gap-[100px] flex-col md:flex-row">
+                <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] flex-col md:flex-row">
                     {/* MENU START */}
-                    <div className="flex flex-col gap-3">
+                    <div className="flex flex-col gap-3 shrink-0">
                         <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
                             Find a store
                         </div>
@@ -28,53 +28,57 @@ const Footer = () => {
                     </div>
                     {/* MENU END */}
 
-                    {/* MENU START */}
-                    <div className="flex flex-col gap-3">
-                        <div className="font-oswald font-medium uppercase text-sm">
-                            get help
+                    {/* NORMAL MENU START */}
+                    <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] shrink-0">
+                        {/* MENU START */}
+                        <div className="flex flex-col gap-3">
+                            <div className="font-oswald font-medium uppercase text-sm">
+                                get help
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Order Status
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Delivery
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Returns
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Payment Options
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Contact Us
+                            </div>
                         </div>
-                        <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                            Order Status
-                        </div>
-                        <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                            Delivery
-                        </div>
-                        <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                            Returns
-                        </div>
-                        <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                            Payment Options
-                        </div>
-                        <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                            Contact Us
-                        </div>
-                    </div>
-                    {/* MENU END */}
+                        {/* MENU END */}
 
-                    {/* MENU START */}
-                    <div className="flex flex-col gap-3">
-                        <div className="font-oswald font-medium uppercase text-sm">
-                            About nike
+                        {/* MENU START */}
+                        <div className="flex flex-col gap-3">
+                            <div className="font-oswald font-medium uppercase text-sm">
+                                About nike
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                News
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Careers
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Investors
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Sustainability
+                            </div>
                         </div>
-                        <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                            News
-                        </div>
-                        <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                            Careers
-                        </div>
-                        <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                            Investors
-                        </div>
-                        <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
-                            Sustainability
-                        </div>
+                        {/* MENU END */}
                     </div>
-                    {/* MENU END */}
+                    {/* NORMAL MENU END */}
                 </div>
                 {/* LEFT END */}
 
                 {/* RIGHT START */}
-                <div className="flex gap-4">
+                <div className="flex gap-4 justify-center md:justify-start">
                     <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
                         <FaFacebookF size={20} />
                     </div>
@@ -92,13 +96,13 @@ const Footer = () => {
             </Wrapper>
             <Wrapper className="flex justify-between mt-10 flex-col md:flex-row gap-[10px] md:gap-0">
                 {/* LEFT START */}
-                <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+                <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer text-center md:text-left">
                     © 2023 Nike, Inc. All Rights Reserved
                 </div>
                 {/* LEFT END */}
 
                 {/* RIGHT START */}
-                <div className="flex gap-5">
+                <div className="flex gap-1 md:gap-5 text-center md:text-left flex-wrap justify-center">
                     <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
                         Guides
                     </div>
