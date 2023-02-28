f6541786d76ee7a320c882460719920dcb70ad6b
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-03-01 00:39:23 +0530

    fixes

--- frontent/pages/index.js
+++ frontent/pages/index.js
@@ -8,10 +8,10 @@ export default function Home() {
             <HomeBanner />
             <Wrapper>
                 <div className="text-center max-w-[800px] mx-auto my-[80px]">
-                    <div className="text-[34px] mb-5 font-semibold leading-tight">
+                    <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
                         Cushioning for Your Miles
                     </div>
-                    <div className="text-xl">
+                    <div className="text-md md:text-xl">
                         A lightweight Nike ZoomX midsole is combined with
                         increased stack heights to help provide cushioning
                         during extended stretches of running.
