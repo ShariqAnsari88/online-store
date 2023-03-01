42b19b9657c060f1d14754c3a3e6f26a8080803e
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-03-02 01:05:38 +0530

    hero banner changes

--- frontent/components/HomeBanner.jsx
+++ frontent/components/HomeBanner.jsx
@@ -32,19 +32,28 @@ const HomeBanner = () => {
                 )}
             >
                 <div>
-                    <img src="/slide-1.png" />
+                    <img
+                        src="/slide-1.png"
+                        className="aspect-[16/10] md:aspect-auto object-cover"
+                    />
                     <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
                         Shop now
                     </div>
                 </div>
                 <div>
-                    <img src="/slide-2.png" />
+                    <img
+                        src="/slide-2.png"
+                        className="aspect-[16/10] md:aspect-auto object-cover"
+                    />
                     <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
                         Shop now
                     </div>
                 </div>
                 <div>
-                    <img src="/slide-3.png" />
+                    <img
+                        src="/slide-3.png"
+                        className="aspect-[16/10] md:aspect-auto object-cover"
+                    />
                     <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
                         Shop now
                     </div>
--- frontent/pages/index.js
+++ frontent/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
             <HomeBanner />
             <Wrapper>
                 {/* HEADING AND PARAGRAPH START */}
-                <div className="text-center max-w-[800px] mx-auto my-[80px]">
+                <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
                     <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
                         Cushioning for Your Miles
                     </div>
