a5ed29921ec49f5a904e250722dafa528b1aedb1
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-03-05 04:04:42 +0530

    t2

--- frontent/pages/category/[category].js
+++ frontent/pages/category/[category].js
@@ -1,6 +1,6 @@
 import React from "react";
 import Wrapper from "@/components/Wrapper";
-import ProductCard from "@/components/ProductCard";
+// import ProductCard from "@/components/ProductCard";
 const Category = () => {
     return (
         <div className="w-full md:py-20">
@@ -15,6 +15,7 @@ const Category = () => {
 
                 {/* PRODUCTS START */}
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+                    {/* <ProductCard />
                     <ProductCard />
                     <ProductCard />
                     <ProductCard />
@@ -22,8 +23,7 @@ const Category = () => {
                     <ProductCard />
                     <ProductCard />
                     <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
+                    <ProductCard /> */}
                 </div>
                 {/* PRODUCTS END */}
             </Wrapper>
