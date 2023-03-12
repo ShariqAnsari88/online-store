233efff22aa1c4ae6fdb1ce925ca56bf280a9e64
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-03-13 02:11:19 +0530

    pagination functionality & success page done

--- frontent/pages/category/[slug].js
+++ frontent/pages/category/[slug].js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import Wrapper from "@/components/Wrapper";
 import ProductCard from "@/components/ProductCard";
 import { fetchDataFromApi } from "@/utils/api";
+import useSWR from "swr";
 
-const Category = ({ category, products }) => {
+const Category = ({ category, products, slug }) => {
+    const [pageIndex, setPageIndex] = useState(1);
+
+    const { data } = useSWR(
+        `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=${pageIndex}&pagination[pageSize]=6`,
+        (url) => fetchDataFromApi(url, true),
+        {
+            fallbackData: products,
+        }
+    );
+
     return (
         <div className="w-full md:py-20">
             <Wrapper>
@@ -17,11 +29,38 @@ const Category = ({ category, products }) => {
 
                 {/* PRODUCTS START */}
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-                    {products?.data?.map((product) => (
+                    {data?.data?.map((product) => (
                         <ProductCard key={product.id} data={product} />
                     ))}
                 </div>
                 {/* PRODUCTS END */}
+
+                {/* PAGINATION BUTTONS START */}
+                <div className="flex gap-3 items-center justify-center my-16 md:my-0">
+                    <button
+                        className={`rounded py-2 px-4 bg-black text-white disabled:bg-gray-200 disabled:text-gray-500`}
+                        disabled={pageIndex === 1}
+                        onClick={() => setPageIndex(pageIndex - 1)}
+                    >
+                        Previous
+                    </button>
+
+                    <span className="font-bold">{`${pageIndex} of ${
+                        data && data.meta.pagination.pageCount
+                    }`}</span>
+
+                    <button
+                        className={`rounded py-2 px-4 bg-black text-white disabled:bg-gray-200 disabled:text-gray-500`}
+                        disabled={
+                            pageIndex ===
+                            (data && data.meta.pagination.pageCount)
+                        }
+                        onClick={() => setPageIndex(pageIndex + 1)}
+                    >
+                        Next
+                    </button>
+                </div>
+                {/* PAGINATION BUTTONS END */}
             </Wrapper>
         </div>
     );
@@ -49,13 +88,14 @@ export async function getStaticProps({ params: { slug } }) {
         `/api/categories?filters[slug][$eq]=${slug}`
     );
     const products = await fetchDataFromApi(
-        `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}`
+        `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=1&pagination[pageSize]=6`
     );
 
     return {
         props: {
             category,
             products,
+            slug,
         },
     };
 }
