2fd24b289f9f52291c69cc3b2bca44ca242c5886
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-03-16 04:57:29 +0530

    localhost url removed

--- frontent/utils/urls.js
+++ frontent/utils/urls.js
@@ -1,4 +1,3 @@
-export const API_URL =
-    process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
